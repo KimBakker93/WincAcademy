ed6b5b020301e170430254f660ab71512b62dcef
KimBakker93 <79530797+KimBakker93@users.noreply.github.com>
2021-05-10 22:25:13 +0200

    Assignment Superpy

---
+++ superpy/main.py
@@ -0,0 +1,171 @@
+# Imports
+import argparse
+import csv
+from rich import print
+from rich.table import Table
+from datetime import datetime, timedelta
+from pathlib import Path
+import plotly.graph_objects as px
+from pandas import read_csv
+from src.report import Report
+
+# Do not change these lines.
+__winc_id__ = 'a2bc36ea784242e4989deb157d527ba0'
+__human_name__ = 'superpy'
+
+
+# Your code below this line.
+
+# Date written to file.
+internal_date = datetime.now().date()
+# Actual current OS date.
+current_date = datetime.now().date()
+report = Report('sold.csv','bought.csv')
+
+def main():
+    global internal_date
+    parser = argparse.ArgumentParser(description = 'Process some integers.')
+    parser.add_argument('type', type = str, help = 'Type of action, must be either buy, sell, report or advance.')
+    parser.add_argument('--report-type', type = str, help = 'Type of report, must be either revenue, profit or inventory.')
+    parser.add_argument('--product-name', type = str, help = 'Name of a product the supermarket offers.')
+    parser.add_argument('--bought-id', type = int, help = 'Id of the sold product as it is declared in the bought.csv file.')
+    parser.add_argument('--price', type = float, help = 'The amount of money per product the supermarket paid for a product or sold it for.')
+    parser.add_argument('--expiration-date', type = str, help = 'The date on which the product the supermarket has in stock expires. The date shows the year first, than the month and lastly the day.')
+    parser.add_argument('--advance-time', default = 0, type = int, help = 'A tool which you can use to fastworward the time to two days later.')
+    parser.add_argument('--date', type = str, help = 'Manner to specify a specific moment in time.')
+    parser.add_argument('--yesterday', action = 'store_true', help = 'Manner to specify a specific moment in time, only used for reports.')
+    internal_date = get_internal_date()
+    args = parser.parse_args()
+
+    # To advance time by one day or more.
+    if args.type == 'advance' and args.advance_time:
+        temp_date = internal_date + timedelta(days=args.advance_time)
+        internal_date = set_internal_date(temp_date)
+        return print('[green][bold]OK[/green][/bold]')
+
+    period = 'Today'
+    check_date = datetime.now().strftime('%Y-%m-%d')
+    if args.yesterday:
+        period = 'Yesterday'
+        check_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+    if args.date:
+        period = args.date
+        check_date = args.date
+
+    if args.type == 'buy':
+        write_buy_to_csv(args.product_name, args.price, args.expiration_date)
+    if args.type == 'sell':
+        write_sell_to_csv(args.bought_id, args.price)
+    if args.type == 'report':
+        if args.report_type == "revenue_chart":
+            report.show_revenue_chart()
+        if args.report_type == 'revenue':
+            report.report_revenue(period,check_date)
+        if args.report_type == 'profit':
+            report.report_profit(period,check_date)
+        if args.report_type == 'inventory':
+            report.report_inventory(period,check_date)    
+        
+
+def get_internal_date():
+    try:
+        f = open('time.txt', 'r')
+        file_date = f.read()
+        return datetime.strptime(file_date, '%Y-%m-%d').date()
+    except FileNotFoundError:
+        f = open('time.txt', 'x')
+        f.write(datetime.now().date().strftime('%Y-%m-%d'))
+        return datetime.now().date()
+    finally:
+        f.close()
+
+def set_internal_date(new_date:datetime):
+    f = open('time.txt', 'w')
+    f.write(new_date.strftime('%Y-%m-%d'))
+    f.close()
+    return new_date
+    
+# Transfer data bought products to CSV-file.    
+def write_buy_to_csv(product_name: str, buy_price: float, expiration_date: str):
+    global current_date
+    id = 1
+
+    # Checks if exists, if not creates the file.
+    Path('bought.csv').touch(exist_ok = True)
+    
+    write_file = open('bought.csv', 'a+', newline = '')
+    read_file = open('bought.csv', 'r', newline = '')
+
+    # Read current Id
+    reader = csv.reader(read_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+    readlist = list(reader)
+
+    # Gives Id-number to bought product.
+    if len(readlist) > 0:
+        id = int(readlist[len(readlist) - 1][0]) + 1
+    read_file.close() 
+    writer = csv.writer(write_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+    if id == 1:
+        writer.writerow(["bought_id", "product_name", "buy_date", "buy_price", "expiration_date", "sold_date"])
+    
+    writer.writerow([id, product_name, current_date.strftime('%Y-%m-%d'), buy_price, expiration_date, None])
+
+    write_file.close() 
+    print('[green]Product has been added to the inventory.[/green]')
+
+# Transfer data sold products to CSV-file.
+def write_sell_to_csv(bought_id: int, sell_price: float):
+    global current_date
+    id = 1
+
+    # Declare files.
+    Path('bought.csv').touch(exist_ok=True)
+    Path('sold.csv').touch(exist_ok=True)
+    sold_read_file = open('sold.csv', 'r+', newline = '') 
+    bought_read_file = open('bought.csv', 'r+', newline = '') 
+
+    # Read current Id
+    reader = csv.reader(sold_read_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+    readlist = list(reader)
+
+    # Gives Id-number to sold product.
+    if len(readlist) > 0:
+        id = int(readlist[len(readlist) - 1][0]) + 1
+    sold_read_file.close()
+
+    # Get associated bought Id
+    bought_reader = csv.reader(bought_read_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+    bought_list = list(bought_reader)
+    product_name = ''
+
+    # Add sold date to item in bought.csv
+    for line, row in enumerate(bought_list):
+        if row[0] == str(bought_id):
+            product_name = row[1]
+            bought_list[line][5] = current_date.strftime('%Y-%m-%d')
+            break
+    bought_read_file.close()
+    
+    # When bought_id not found
+    if product_name == '':
+        print('[red]Error: Product not found in the inventory.[red]')
+        return
+    
+    bought_write_file = open('bought.csv', 'w+', newline='') 
+    bought_writer = csv.writer(bought_write_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+    bought_writer.writerows(bought_list)
+
+    bought_write_file.close()
+
+    sold_write_file = open('sold.csv', 'a+', newline='') 
+
+    writer = csv.writer(sold_write_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+    if id == 1:
+        writer.writerow(["sold_id", "bought_id", "product_name", "sold_date", "sell_price"])
+    
+    writer.writerow([id, bought_id, product_name, current_date.strftime('%Y-%m-%d'), sell_price])  
+    sold_write_file.close() 
+    print('[green]Product has been removed from the inventory.[/green]')
+
+if __name__ == '__main__':
+    main()
---
+++ superpy/src/report.py
@@ -0,0 +1,95 @@
+from rich import print
+from rich.table import Table
+from datetime import datetime, timedelta
+from pathlib import Path
+import plotly.graph_objects as px
+from pandas import read_csv
+import csv
+
+class Report:
+    bought_csv = ''
+    sold_csv = ''
+
+    def __init__ (self,sold_csv,bought_csv):
+        self.sold_csv = sold_csv
+        self.bought_csv = bought_csv
+    
+    # Reports revenue of moment or span of time.
+    def report_revenue(self, date_str:str, check_date:str):
+        sold_read_file = open(self.sold_csv, 'r+', newline = '')
+
+        # Read current Id
+        reader = csv.reader(sold_read_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+        sold_list = list(reader)
+        
+        # Adding sellprices.
+        revenue = 0
+        for line, row in enumerate(sold_list):
+            if row[3].startswith(check_date):
+                revenue += float(row[4])
+        print(date_str + "'s revenue is: [green][bold]" + str(round(revenue,2)) + "[/green][/bold]")        
+
+    # Reports profit of certain date. 
+    def report_profit(self, date_str:str, check_date_str:str):
+        sold_read_file = open(self.sold_csv, 'r+', newline = '')
+        bought_read_file = open(self.bought_csv, 'r+', newline = '') 
+
+        # Read current Id
+        reader = csv.reader(sold_read_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+        bought_reader = csv.reader(bought_read_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+        sold_list = list(reader)
+        
+        # Amount sold minus amount bought.
+        profit = 0
+        for line, row in enumerate(sold_list):
+            if row[3].startswith(check_date_str):
+                for bought_row in bought_reader:
+                    if bought_row[0] == row[1]:
+                        profit -= float(bought_row[3])
+                profit += float(row[4])
+        print(date_str + "'s profit is: [green][bold]" + str(round(profit,2)) + "[/green][/bold]")  
+        sold_read_file.close()
+        bought_read_file.close()      
+
+    # Reports inventory of certain date.
+    def report_inventory(self, date_str:str, check_date_str:str):
+
+        # Table lay-out.
+        table = Table(title = "Inventory " + check_date_str)
+        table.add_column("Id", style = "yellow")
+        table.add_column("Product Name", style = "blue")
+        table.add_column("Buy Date", style = "white")
+        table.add_column("Buy Price", style = "red")
+        table.add_column("Expiration Date", style = "green")
+        bought_read_file = open(self.bought_csv, 'r+', newline = '')
+
+        # Read current Id
+        reader = csv.reader(bought_read_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+        bought_list = list(reader)
+        
+        # Bought products minus sold items.
+        check_date = datetime.strptime(check_date_str, '%Y-%m-%d')
+        for line, row in enumerate(bought_list):
+            if line == 0:
+                continue
+            expire_date = datetime.strptime(row[4], '%Y-%m-%d')
+            buy_date = datetime.strptime(row[2], '%Y-%m-%d')
+            if row[5] != '':
+                sold_date = datetime.strptime(row[5], '%Y-%m-%d')
+                if check_date > sold_date:
+                    continue
+            
+        # Remove expired products from inventory.        
+            if check_date < expire_date and check_date >= buy_date:
+                table.add_row(row[0], row[1], row[2], row[3], row[4])
+        print(table)        
+
+    # Chart revenue.
+    def show_revenue_chart(self):
+        data = read_csv(self.sold_csv, header = 0, index_col = 0, parse_dates = True, squeeze = True)
+        print(data)
+        fig = px.Figure(px.Bar(x = data['sold_date'], y = data['sell_price']))
+        fig.update_layout(title="Revenue", xaxis_tickformat = '%Y-%m-%d')
+        # fig = px.bar(data, x = "sold_date", y="sell_price", title = "Revenue", xaxis_tickformat = '%Y-%m-%d')
+        fig.show()
+        print('[blue]Opening chart in browser.[/blue]')
